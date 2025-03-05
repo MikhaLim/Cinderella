44288231a5d39b494946e3acdc98a6d72d86fdd7
MikhaLim <110205662+MikhaLim@users.noreply.github.com>
2025-03-05 16:11:43 +0800

    Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,15 @@
-ka
+function ToVerifyaMessage () {
+    let input = prompt('Input a message');
+
+    if (input.indexOf('FR') !==  -1) {
+        document.getElementById('result').innerHTML = 'The message "' + input + '" is legitimate!';
+    } else if (input.indexOf('FR') !==  -1 && input.indexOf('AI') !==  -1) {
+        document.getElementById('result').innerHTML = 'The message "' + input + '" is legitimate!';
+    } else if (input.indexOf('AI') !== -1) {
+        document.getElementById('result').innerHTML = 'The message "' + input + '" is fake!';
+    } else if (input.indexOf('aI') !==  -1 || input.indexOf('Ai') !==  -1) {
+        document.getElementById('result').innerHTML = 'The message "' + input + '" is fake!';
+    } else {
+        document.getElementById('result').innerHTML = 'The message "' + input + '" is not yet encoded!';
+    }
+}
