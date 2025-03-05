3b529b79b730e03e2b45dae40ec56f79d28a273a
MikhaLim <110205662+MikhaLim@users.noreply.github.com>
2025-03-05 16:29:38 +0800

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,13 +1,11 @@
 function checkMessage() {
     let message = prompt("Enter a message:");
 
-    if (message.includes("FR") && message.includes("AI")) {
+    if (message.indexOf("FR") !== -1 && message.indexOf("AI") !== -1) {
         document.getElementById("output").innerHTML = `The message "${message}" is legitimate!`;
-    } else if (message.includes("FR")) {
+    } else if (message.indexOf("FR") !== -1) {
         document.getElementById("output").innerHTML = `The message "${message}" is legitimate!`;
-    } else if (message.includes("AI")) {
-        document.getElementById("output").innerHTML = `The message "${message}" is tampered with!`;
-    } else if (message.includes("aI") || message.includes("Ai")) {
+    } else if (message.indexOf("AI") !== -1 || message.indexOf("aI") !== -1 || message.indexOf("Ai") !== -1) {
         document.getElementById("output").innerHTML = `The message "${message}" is tampered with!`;
     } else {
         document.getElementById("output").innerHTML = `The message "${message}" is not yet encoded!`;
