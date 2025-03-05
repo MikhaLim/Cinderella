0d712d76f610b09d691d66264b33c3416049e70e
MikhaLim <110205662+MikhaLim@users.noreply.github.com>
2025-03-05 19:43:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function checkMessage() {
     } else if (message.indexOf("FR") !== -1) {
         document.getElementById("output").innerHTML = `The message "${message}" is legitimate!`;
     } else if (message.indexOf("AI") !== -1 || message.indexOf("aI") !== -1 || message.indexOf("Ai") !== -1) {
-        document.getElementById("output").innerHTML = `The message "${message}" is tampered with!`;
+        document.getElementById("output").innerHTML = `The message "${message}" is tampered`;
     } else {
         document.getElementById("output").innerHTML = `The message "${message}" is not yet encoded!`;
     }
